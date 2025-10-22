cde1170dab0d7c3434e7909b60da1a6ce59b2424
Vitaxses <151165512+Vitaxses@users.noreply.github.com>
2025-10-22 12:25:55 +0200

    Fix bug where Songclave would activate without activating the bellshrine

--- FSMEdits/FasterBossAndNPC.cs
+++ FSMEdits/FasterBossAndNPC.cs
@@ -19,10 +19,14 @@ internal static class FasterBossAndNpc
             return;
 
         if (fsm.gameObject is { name: "Seamstress", scene.name: "Bone_East_Umbrella" }) {
+
             fsm.GetState("DLG After Dress")!.DisableAction(0);
             fsm.gameObject.transform.Find("Exit Lore Wall").localPosition = new Vector3(-30f, 1.91f, 0f);
+
         } else if (fsm.gameObject is { name: "Enclave Caretaker FirstMeet", scene.name: "Song_Enclave" }) {
-            PlayerData.instance.metCaretaker = true;
+
+            if (PlayerData.instance.bellShrineEnclave)
+                PlayerData.instance.metCaretaker = true;
         }
     }
 }
