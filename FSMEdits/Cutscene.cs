27fa95aa711a23be58118fee55e84ebf9d1e5db1
Frank Yang <clazex.y@gmail.com>
2025-10-27 16:27:53 +0800

    Skip Last Dive

---
+++ FSMEdits/Cutscene.cs
@@ -0,0 +1,20 @@
+﻿using HutongGames.PlayMaker.Actions;
+
+namespace QoL.FSMEdits;
+
+internal static class Cutscene
+{
+    internal static void LastDive(PlayMakerFSM fsm)
+    {
+        if (!Configs.SkipCutscene.Value)
+            return;
+
+        if (fsm is not { FsmName: "Dive Cutscene", gameObject: { name: "Abyss Dive Cutscene", scene.name: "Abyss_05" } })
+            return;
+
+        fsm.DisableAction("Preload Scene", 3); // Don't Preload
+        BeginSceneTransition bst = fsm.GetAction<BeginSceneTransition>("Dive End", 9)!;
+        bst.sceneName = "Abyss_Cocoon";
+        bst.entryGateName = "door_entry";
+    }
+}
